52ff01c61b19d5e2a844f4c5097af37de906ca3f
Adam Zinder and Chris Clifford <chris@>
2016-10-26 11:47:00 -0700

    implement if statement that looks into length of consonants in a row

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,40 @@
 var vowels = ['a', 'e', 'i', 'o', 'u'];
 var consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
-var embededSymbols = [];
-var tailingSymbols = ['.', ',', ';', ':']
 
 var pigLatin = function (input){
+  var consanantRep = []
   var inputNoSymbols = input.replace(/[^a-zA-Z ]/g, "").toLowerCase();
   var letters = inputNoSymbols.split("");
+  var inputLength = input.length;
   console.log(letters);
   if (vowels.includes(letters[0])){
     letters.push("a", "y");
-    alert(letters);
+    //alert(letters);
   }
   else {
+    for (idx = 0; idx < letters.length; idx ++)
+      if (consonants.includes(letters[idx])){
+        consanantRep.push(letters[idx]);
+      }
+      else {
+        break
+      }
     //var firstLetter = letters[0];
+    //letters.shift()
+    //letters.push(firstLetter, "a", "y");
 
-    letters.push("a", "y");
+
+    alert(letters);
   }
+  //return letters.toString("");
 
 };
 
-$(function(){
+$(document).ready(function(){
   $('.blanks form').submit(function(event){
+    event.preventDefault();
     var inputSentence = $('input#sentence').val();
     var pigSentence = pigLatin(inputSentence);
-    event.preventDefault();
+
   });
 });
