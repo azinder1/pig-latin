71e76a0014f7ad97b910e7a37a4ac06369c54b29
Adam Zinder and Chris Clifford <chris@>
2016-10-26 14:45:24 -0700

    add code that displays the correctly translated sentence, but doesnt handle symbols

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,55 @@
 var vowels = ['a', 'e', 'i', 'o', 'u'];
 var consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
+var tailingSymbols = [',','.',';',':','!','?',")",'}',"]"]
+//This funciton
+var pigLatinSentence = function(sentenceInput) {
+  var words = sentenceInput.split(" ");
+  var pigLatinFinal = [];
+  words.forEach(function(word) {
+    pigLatinFinal.push(pigLatin(word));
 
+  });
+    return pigLatinFinal.join(" ");
+};
+
+
+
+//This function takes a word and returns pig latin version of that word
 var pigLatin = function (input){
-  var inputNoSymbols = input.replace(/[^a-zA-Z ]/g, "").toLowerCase();
+  //replace(/[^a-zA-Z ]/g, "")
+  var inputNoSymbols = input.toLowerCase();
   var letters = inputNoSymbols.split("");
   var inputLength = input.length;
-  console.log(letters);
   if (vowels.includes(letters[0])){
     letters.push("a", "y");
-    //alert(letters);
   }
   else {
-    debugger
     for (idx = 0; idx < letters.length; idx ++)
-      if (consonants.includes(letters[0])){
+      if (letters[0]=== "q" && letters[1] === "u") {
+        letters.push(letters.shift());
+        letters.push(letters.shift());
+      }
+      else if (consonants.includes(letters[0])){
 
         letters.push(letters.shift());
+        if (letters[0] === "y") {
+          letters.push("a", "y");
+          break
+        }
       }
       else {
         letters.push("a", "y");
         break
       }
-
-    //var firstLetter = letters.shift();
-    //letters.push(firstLetter, "a", "y");
-
-
-    alert(letters);
   }
-  //return letters.toString("");
-
+  return letters.join("");
 };
 
 $(document).ready(function(){
   $('.blanks form').submit(function(event){
     event.preventDefault();
-    var inputSentence = $('input#sentence').val();
-    var pigSentence = pigLatin(inputSentence);
-
+    var inputWord = $('input#sentence').val();
+    var pigWord = pigLatinSentence(inputWord);
+    $('#output h5').text(pigWord);
   });
 });
